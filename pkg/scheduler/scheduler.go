0e07884a29528f98a729d46abe9fe165499f9d68
chendan <doke.hi@gmail.com>
2024-07-11 14:40:53 +0800

    detail gpu Insufficient info

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -480,7 +480,7 @@ func (s *Scheduler) Filter(args extenderv1.ExtenderArgs) (*extenderv1.ExtenderFi
 		s.recordScheduleFilterResultEvent(args.Pod, EventReasonFilteringFailed, []string{}, fmt.Errorf("no available node, all node scores do not meet"))
 		return &extenderv1.ExtenderFilterResult{
 			FailedNodes: failedNodes,
-			Error:       fmt.Sprintf("Insufficient %s", gpuType),
+			Error:       fmt.Sprintf("0/%v nodes are available: %d nodes Insufficient %s.", len(*args.NodeNames), len(*args.NodeNames), gpuType),
 		}, nil
 	}
 	klog.V(4).Infoln("nodeScores_len=", len((*nodeScores).NodeList))
