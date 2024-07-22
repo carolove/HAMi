f16e206ceb95c97551dc3a572fefc285400b504b
chendan <doke.hi@gmail.com>
2024-07-22 11:17:06 +0800

    add libcuda log level for libvgpu

--- pkg/util/types.go
+++ pkg/util/types.go
@@ -86,6 +86,7 @@ var (
 	// DevicePluginFilterDevice need device-plugin filter this device, don't register this device.
 	DevicePluginFilterDevice *FilterDevice
 	CustomModelName          *string
+	LibcudaLogLevel          *uint // set libvgpu LIBCUDA_LOG_LEVEL
 )
 
 //	type ContainerDevices struct {
