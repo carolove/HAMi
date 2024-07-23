885cca6f6b0da81c03d4d47bd795f7946d10a18f
chendan <doke.hi@gmail.com>
2024-07-23 10:03:37 +0800

    fix filter result error

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -481,7 +481,7 @@ func (s *Scheduler) Filter(args extenderv1.ExtenderArgs) (*extenderv1.ExtenderFi
 		s.recordScheduleFilterResultEvent(args.Pod, EventReasonFilteringFailed, []string{}, fmt.Errorf("no available node, all node scores do not meet"))
 		return &extenderv1.ExtenderFilterResult{
 			FailedNodes: failedNodes,
-			Error:       fmt.Sprintf("0/%v nodes are available: %d nodes Insufficient %s.", len(*args.NodeNames), len(*args.NodeNames), gpuType),
+			Error:       fmt.Sprintf(" nodes: %v, %d nodes Insufficient %s.", *args.NodeNames, len(*args.NodeNames), gpuType),
 		}, nil
 	}
 	klog.V(4).Infoln("nodeScores_len=", len((*nodeScores).NodeList))
