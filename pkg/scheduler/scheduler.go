d11a6b73f279fa12cdc5d799d357187c7ed2bf29
chendan <doke.hi@gmail.com>
2024-07-23 11:17:50 +0800

    without devicememoryscaling full filter Insufficient info

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -481,7 +481,7 @@ func (s *Scheduler) Filter(args extenderv1.ExtenderArgs) (*extenderv1.ExtenderFi
 		s.recordScheduleFilterResultEvent(args.Pod, EventReasonFilteringFailed, []string{}, fmt.Errorf("no available node, all node scores do not meet"))
 		return &extenderv1.ExtenderFilterResult{
 			FailedNodes: failedNodes,
-			Error:       fmt.Sprintf(" nodes: %v, %d nodes Insufficient %s.", *args.NodeNames, len(*args.NodeNames), gpuType),
+			Error:       fmt.Sprintf(" nodes: %v, %d nodes Insufficient %s. other nodes Insufficient cpu/memory/others", *args.NodeNames, len(*args.NodeNames), gpuType),
 		}, nil
 	}
 	klog.V(4).Infoln("nodeScores_len=", len((*nodeScores).NodeList))
